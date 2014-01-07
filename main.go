44e45fd08efb8f86889188c43d483cca98de4b12
giulio <giuliop@gmail.com>
2014-01-07 11:42:33 +0100

    made user messages a bit more clear

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ import (
 func help(s string) {
 	fmt.Println(s + "\n")
 	fmt.Println("Usage: gouninstall <package> [<path>]")
-	fmt.Println("If <path> is omitted then the first path in $GOPATH is taken\n")
+	fmt.Println("<package> is the package name including import path to be uninstalled")
+	fmt.Println("<path> is to be used if you have more than one path in your $GOPATH. If omitted the first path in $GOPATH is taken\n")
+	fmt.Println("Please note that this utility will not look in your GOROOT and assume packages are installed in your $GOPATH")
 }
 
 // buildArgs build the list of dirs/files to delete
@@ -43,29 +45,35 @@ func buildArgs(pkg, dir string) (string, []string) {
 // uninstall delete the dirs/files of the package
 func uninstall(pkg string, dirs []string) {
 	var okdir = make([]bool, 4)
+	numFileToDelete := 0
 	for i, d := range dirs {
 		var dtype string
 		switch i {
 		case 0, 1:
-			dtype = "Directory"
+			dtype = "directory"
 		case 2, 3:
-			dtype = "File"
+			dtype = "file"
 		default:
 			panic("Too many arguments!")
 		}
 		dinfo, err := os.Stat(d)
 		if err != nil {
 			if os.IsNotExist(err) {
-				fmt.Printf("%s %s not found\n", dtype, d)
+				fmt.Printf("Looking for %s %s ... does not exist\n", dtype, d)
 			} else {
 				fmt.Printf("Error - %s\n", err)
 			}
 		} else if i < 2 && !dinfo.IsDir() {
 			fmt.Printf("Error: %d is a file, not a directory\n")
 		} else {
-			fmt.Printf("%s %s will be DELETED\n", dtype, d)
+			fmt.Printf("Looking for %s %s ... found, will be DELETED\n", dtype, d)
 			okdir[i] = true
+			numFileToDelete++
 		}
+	}
+	if numFileToDelete == 0 {
+		fmt.Println("\nNothing to delete")
+		return
 	}
 	fmt.Println("\nDo you want to proceed? Type y/n")
 	var input string
